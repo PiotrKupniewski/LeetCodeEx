e5adb1a64d5715d41dc81f36d3a67c5a4a1e03b2
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-10-29 01:16:36 +0100

    started working on and playing with algorythmic exercises

---
+++ src/main/java/Ex1/TwoSum.java
@@ -0,0 +1,20 @@
+package Ex1;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        int length = nums.length;
+        //sort numbers
+        Arrays.sort(nums);
+
+        //get intdex of number >= target
+        int
+        for(int i =0; i<nums.length;i++){
+
+        }
+
+        return null;
+    }
+}
