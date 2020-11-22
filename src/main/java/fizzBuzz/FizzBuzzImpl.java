b045e8cdb2325b36e69a5774ab8d928bcc09639a
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-22 14:11:23 +0100

    fizz buzz impl '

--- src/main/java/fizzBuzz/FizzBuzzImpl.java
+++ src/main/java/fizzBuzz/FizzBuzzImpl.java
@@ -1,4 +1,19 @@
 package fizzBuzz;
 
 public class FizzBuzzImpl {
+
+    public static void fizzBuzz(){
+        for(var i = 1000; i<= 1300; i++){
+
+            String out =  i + "-- ";
+            if(i%3 == 0){
+                out += "Fizz";
+            }
+            if(i%5 == 0){
+                out +="Buzz";
+            }
+            System.out.println("out : {} "+  out );
+        }
+    }
+
 }
