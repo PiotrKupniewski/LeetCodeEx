da71fc9a88c74693fb7383543a1743ac4b0139eb
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-11-22 14:11:08 +0100

    fiz buz test"
    "

---
+++ src/test/java/fizzBuzzTest/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+package fizzBuzzTest;
+
+import fizzBuzz.FizzBuzzImpl;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void printFizzBuzzOut(){
+        FizzBuzzImpl.fizzBuzz();
+    }
+}
